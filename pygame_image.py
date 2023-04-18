ca58b63a0c891917b7817e240206a7fa8946efe3
shimizu <c0b21082ef@edu.teu.ac.jp>
2023-04-18 18:32:23 +0900

    課題　未完成

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     bird_img = pg.image.load("ex01/fig/3.png")
     bird_img = pg.transform.flip(bird_img, True, False)
     bird_img2 = pg.transform.rotozoom(bird_img, 10, 1.0)
@@ -14,6 +15,9 @@ def main():
     tmr = 0
     bird_fly = 0
     bg_img_go = 0
+    whe_i = 0
+    x = 0
+    y = 0
 
     while True:
         for event in pg.event.get():
@@ -21,12 +25,13 @@ def main():
 
         tmr += 1
         bg_img_go = tmr % 1600
+        whe_i = tmr % 3200
 
         if tmr % 100 == 0:
             bird_fly += 1
 
         screen.blit(bg_img, [-bg_img_go, 0])
-        screen.blit(bg_img, [1600 - bg_img_go, 0])
+        screen.blit(bg_img2, [1600 - whe_i, 0])
         screen.blit(bird_list[bird_fly % 2], (300, 200))
 
 
