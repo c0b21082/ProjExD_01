4acc97f40b0a5e38dbee68188bcd31b97198fddf
shimizu <c0b21082ef@edu.teu.ac.jp>
2023-04-18 15:34:23 +0900

    練習問題5まで

---
+++ pygame_image.py
@@ -0,0 +1,33 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bird_img = pg.image.load("ex01/fig/3.png")
+    bird_img = pg.transform.flip(bird_img, True, False)
+    bird_img2 = pg.transform.rotozoom(bird_img, 10, 1.0)
+    bird_list = [bird_img, bird_img2]
+
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+        screen.blit(bird_list[tmr % 2], (300, 200))
+
+
+        pg.display.update()
+        clock.tick(100)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
